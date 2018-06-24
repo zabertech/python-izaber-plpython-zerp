4bf0a574dc1dde34454b9032e9d84d99307367ef
Aki Mimoto <aki@bako.ca>
2018-06-24 00:09:02 -0700

    bump version

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@
 from setuptools import setup
 
 setup(name='izaber_plpython_zerp',
-      version='1.2',
+      version='1.3',
       description='Base load point for iZaber plpython for Zerp',
       url = 'https://github.com/zabertech/izaber-plpython-zerp',
-      download_url = 'https://github.com/zabertech/python-plpython-zerp/archive/1.2.tar.gz',
+      download_url = 'https://github.com/zabertech/python-plpython-zerp/archive/1.3.tar.gz',
       author='Aki Mimoto',
       author_email='aki+izaber@zaber.com',
       license='MIT',
